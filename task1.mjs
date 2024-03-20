e1629d0dca3a7c20fb4937f4e903fbd4fb226053
Lily_Pandova <liliapandova1@gmail.com>
2024-03-20 16:27:50 +0200

    [Task3]; added the consoles

--- task1.mjs
+++ task1.mjs
@@ -63,7 +63,7 @@ class MyEventEmitter {
 class WithTime extends MyEventEmitter {
     execute(asyncFunc, ...args) {
         const startTime = new Date();
-        this.emit("start"); // or "begin"?
+        this.emit("begin"); // or "start"?
 
         const cbFunc = (response) => {
             let data = {};
@@ -97,3 +97,64 @@ const fetchFromUrl = (url, cb) => {
    
     https.get(url, options, cb);
 }
+
+
+// Task 1
+
+const myEmitter = new MyEventEmitter();
+
+function c1() {
+    console.log('an event occurred!');
+}
+
+function c2() {
+    console.log('yet another event occurred!');
+}
+
+myEmitter.on('eventOne', c1); // Register for eventOne
+myEmitter.on('eventOne', c2); // Register for eventOne
+
+// Register eventOnce for one time execution
+myEmitter.once('eventOnce', () => console.log('eventOnce once fired'));
+myEmitter.once('init', () => console.log('init once fired'));
+
+// Register for 'status' event with parameters
+myEmitter.on('status', (code, msg)=> console.log(`Got ${code} and ${msg}`));
+
+
+myEmitter.emit('eventOne');
+
+// Emit 'eventOnce' -> After this the eventOnce will be
+// removed/unregistered automatically
+myEmitter.emit('eventOnce');
+
+
+myEmitter.emit('eventOne');
+myEmitter.emit('init');
+myEmitter.emit('init'); // Will not be fired
+myEmitter.emit('eventOne');
+myEmitter.emit('status', 200, 'ok');
+
+// Get listener's count
+console.log(myEmitter.listenerCount('eventOne'));
+
+// Get array of rawListeners//
+// Event registered with 'once()' will not be available here after the
+// emit has been called
+console.log(myEmitter.rawListeners('eventOne'));
+
+// Get listener's count after remove one or all listeners of 'eventOne'
+myEmitter.off('eventOne', c1);
+console.log(myEmitter.listenerCount('eventOne'));
+myEmitter.off('eventOne', c2);
+console.log(myEmitter.listenerCount('eventOne'));
+
+// Task 2
+const withTime = new WithTime();
+
+withTime.on('begin', () => console.log('About to execute'));
+withTime.on('end', () => console.log('Done with execute'));
+
+withTime.execute(fetchFromUrl, 'https://jsonplaceholder.typicode.com/posts/1');
+
+console.log(withTime.rawListeners("end"));
