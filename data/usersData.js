a4c2a09c72af814581c3659b6a7451b57bea6917
Lily_Pandova <liliapandova1@gmail.com>
2024-03-25 21:30:28 +0200

    [task5]: server, utils, data

---
+++ data/usersData.js
@@ -0,0 +1,106 @@
+export let usersData =  [
+	{
+		"user": {
+			"id": "abacf8f3-570b-45be-85f6-e5ff8db45bac",
+			"name": "John Doe",
+			"email": "johndoe@example.com"
+		},
+		"links": {
+			"self": "/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98",
+			"hobbies": [
+				"dancing",
+				"gardening",
+				"hiking"
+			]
+		}
+	},
+	{
+		"user": {
+			"id": "8c67d352-21ce-4a10-af27-278fd00ecf93",
+			"name": "Test user",
+			"email": "kirova@gmail.com"
+		},
+		"links": {
+			"self": "/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98",
+			"hobbies": "/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98/hobbies"
+		}
+	},
+	{
+		"user": {
+			"id": "5ba348d6-d32f-43fc-a937-dfe29d9b08c8",
+			"name": "Test user-2",
+			"email": "kirova@gmail.com"
+		},
+		"links": {
+			"self": "/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98",
+			"hobbies": [
+				"/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98/hobbies",
+				"sports"
+			]
+		}
+	},
+	{
+		"user": {
+			"id": "b013b125-1e7b-4737-9537-f4c5e4cc6bae",
+			"name": "Test user-3",
+			"email": "kirova@gmail.com"
+		},
+		"links": {
+			"self": "/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98",
+			"hobbies": [
+				"hiking",
+				"dancing",
+				"sports",
+				"gardening"
+			]
+		}
+	},
+	{
+		"user": {
+			"id": "36fba794-e523-407c-b693-28fb3122cbd9",
+			"name": "John Doe - test",
+			"email": "johndoe@example.com"
+		},
+		"links": {
+			"self": "/api/users/36fba794-e523-407c-b693-28fb3122cbd9",
+			"hobbies": "/api/users/36fba794-e523-407c-b693-28fb3122cbd9/hobbies"
+		}
+	},
+	{
+		"user": {
+			"id": "452439e3-c788-417c-be78-42728a7dc970",
+			"name": "John Doe - 3",
+			"email": "johndoe@example.com"
+		},
+		"links": {
+			"self": "/api/users/452439e3-c788-417c-be78-42728a7dc970",
+			"hobbies": [
+				"/",
+				"a",
+				"p",
+				"i",
+				"u",
+				"s",
+				"e",
+				"r",
+				"4",
+				"5",
+				"2",
+				"3",
+				"9",
+				"-",
+				"c",
+				"7",
+				"8",
+				"1",
+				"b",
+				"d",
+				"0",
+				"h",
+				"o",
+				"/api/users/5f3b4b29-03dd-4ed9-84a3-6dfcfz4c2be98/hobbies",
+				"sports"
+			]
+		}
+	}
+]
